d325d8f2ab0fc2080f77d0234360a544b7d17690
jbjouvin <jb.jouvin@gmail.com>
2017-11-13 20:12:30 +0100

    finish docker config

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,6 @@
 # project/__init__.py
 
-
+import os
 from flask import Flask, jsonify
 
 
@@ -8,7 +8,13 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+# app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+
+print("coucou")
+print(app.config)
 
 
 @app.route('/ping', methods=['GET'])
