0e23ae7c63a1ac5670f69c8a45fcd43423f91621
jbjouvin <jb.jouvin@gmail.com>
2017-11-14 08:49:36 +0100

    app => create_app()

--- manage.py
+++ manage.py
@@ -4,18 +4,11 @@ import unittest
 
 from flask_script import Manager
 
-from project import app, db
+from project import create_app, db
+from project.api.models import User
 
-
+app = create_app()
 manager = Manager(app)
-
-
-@manager.command
-def recreate_db():
-    """Recreates a database."""
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
 
 
 @manager.command
@@ -28,5 +21,14 @@ def test():
     return 1
 
 
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
 if __name__ == '__main__':
+
     manager.run()
