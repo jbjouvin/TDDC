3d7fb3811aaf1d940e3dbebd4134cab0771a5e49
jbjouvin <jb.jouvin@gmail.com>
2017-11-16 15:00:57 +0100

    + jwt

--- project/tests/test_config.py
+++ project/tests/test_config.py
@@ -26,6 +26,8 @@ class TestDevelopmentConfig(TestCase):
             os.environ.get('DATABASE_URL')
         )
         self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 4)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 30)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 0)
 
 
 class TestTestingConfig(TestCase):
@@ -43,6 +45,8 @@ class TestTestingConfig(TestCase):
             os.environ.get('DATABASE_TEST_URL')
         )
         self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 4)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 0)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 3)
 
 
 class TestProductionConfig(TestCase):
@@ -51,10 +55,15 @@ class TestProductionConfig(TestCase):
         return app
 
     def test_app_is_production(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(
+            app.config['SECRET_KEY'] ==
+            os.environ.get('SECRET_KEY')
+        )
         self.assertFalse(app.config['DEBUG'])
         self.assertFalse(app.config['TESTING'])
         self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 13)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 30)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 0)
 
 
 if __name__ == '__main__':
