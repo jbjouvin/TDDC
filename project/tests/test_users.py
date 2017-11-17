cc806ffcc61120a9c2cba757da58357b382fe81c
jbjouvin <jb.jouvin@gmail.com>
2017-11-17 12:37:36 +0100

    Update test case with admin parma to True

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -24,6 +24,10 @@ class TestUserService(BaseTestCase):
     def test_add_user(self):
         """Ensure a new user can be added to the database."""
         add_user('test', 'test@test.com', 'test')
+        # update user
+        user = User.query.filter_by(email='test@test.com').first()
+        user.admin = True
+        db.session.commit()
         resp_login = self.client.post(
             '/auth/login',
             data=json.dumps(dict(
@@ -55,6 +59,10 @@ class TestUserService(BaseTestCase):
     def test_add_user_invalid_json(self):
         """Ensure error is thrown if the JSON object is empty."""
         add_user('test', 'test@test.com', 'test')
+        # update user
+        user = User.query.filter_by(email='test@test.com').first()
+        user.admin = True
+        db.session.commit()
         resp_login = self.client.post(
             '/auth/login',
             data=json.dumps(dict(
@@ -82,6 +90,11 @@ class TestUserService(BaseTestCase):
     def test_add_user_invalid_json_keys(self):
         """Ensure error is thrown if the JSON object does not have a username key."""
         add_user('test', 'test@test.com', 'test')
+        # update user
+        user = User.query.filter_by(email='test@test.com').first()
+        user.admin = True
+        db.session.commit()
+
         resp_login = self.client.post(
             '/auth/login',
             data=json.dumps(dict(
@@ -109,6 +122,11 @@ class TestUserService(BaseTestCase):
     def test_add_user_duplicate_email(self):
         """Ensure error is thrown if the email already exists."""
         add_user('test', 'test@test.com', 'test')
+        # update user
+        user = User.query.filter_by(email='test@test.com').first()
+        user.admin = True
+        db.session.commit()
+
         resp_login = self.client.post(
             '/auth/login',
             data=json.dumps(dict(
@@ -205,6 +223,11 @@ class TestUserService(BaseTestCase):
     def test_add_user_invalid_json_keys_no_password(self):
         """Ensure error is thrown if the JSON object does not have a password key."""
         add_user('test', 'test@test.com', 'test')
+        # update user
+        user = User.query.filter_by(email='test@test.com').first()
+        user.admin = True
+        db.session.commit()
+
         resp_login = self.client.post(
             '/auth/login',
             data=json.dumps(dict(
@@ -230,6 +253,38 @@ class TestUserService(BaseTestCase):
             self.assertEqual(response.status_code, 400)
             self.assertIn('Invalid payload.', data['message'])
             self.assertIn('fail', data['status'])
+
+    def test_add_user_not_admin(self):
+        add_user('test', 'test@test.com', 'test')
+        with self.client:
+            # user login
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps(dict(
+                    email='test@test.com',
+                    password='test'
+                )),
+                content_type='application/json'
+            )
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='michael',
+                    email='michael@realpython.com',
+                    password='test'
+                )),
+                content_type='application/json',
+                headers=dict(
+                    Authorization='Bearer ' + json.loads(
+                        resp_login.data.decode()
+                    )['auth_token']
+                )
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'error')
+            self.assertTrue(
+                data['message'] == 'You do not have permission to do that.')
+            self.assertEqual(response.status_code, 401)
 
     # def test_main_no_users(self):
     #     """Ensure the main route behaves correctly when no users have been
